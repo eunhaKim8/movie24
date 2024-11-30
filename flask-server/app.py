24ee6b497d2b1a90eed98a9ca6c804e9159f0bea
eunhaKim8 <dmsgk8383@naver.com>
2024-12-01 00:26:14 +0900

    11-30 간단 api 가져오기 테스트

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from flask import Flask, requests, jsonify
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-  return 'this is home'
+# TMDB API KEY
+API_KEY =  "953c7f3db1b660a32c0598a36ad9fb95"
 
-@app.route('/movies', methods=['GET'])
-def get_movies():
-  return {"movies": ["movie1", "movie2", "movie3"]}
+@app.route('/genres', methods=['GET'])
+def get_genres():
+  url = f"https://api.themoviedb.org/3/genre/movie/list?language=ko-KR&api_key={API_KEY}"
+  response = requests.get(url)
+  if response.status_code == 200:
+    return jsonify(response.json())
+  else:
+    return jsonify({"error": response.text}), response.status_code
+
+@app.route('/movies/popular', methods=['GET'])
+def get_movies_popular():
+  url = f"https://api.themoviedb.org/3/movie/popular?language=ko-KR&api_key={API_KEY}"
+  response = requests.get(url)
+  if response.status_code == 200:
+    return jsonify(response.json())
+  else:
+    return jsonify({"error": response.text}), response.status_code
+
+@app.route('/movies/now_playing', methods=['GET'])
+def get_movies_now_playing():
+  url = f"https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&api_key={API_KEY}"
+  response = requests.get(url)
+  if response.status_code == 200:
+    return jsonify(response.json())
+  else:
+    return jsonify({"error": response.text}), response.status_code
+  
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5001, debug=True)
